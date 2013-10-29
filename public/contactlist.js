4129ef6737c366f3b5e01690efa740948abbc959
Marije Rooze <marijer@gmail.com>
2013-10-28 17:11:06 -0700

    updated to point 4

--- public/contactlist.js
+++ public/contactlist.js
@@ -1,12 +1,14 @@
 
 
-function makeContactRowClickable() {
-  $(".table > tbody > tr").click(function() {
-    var newUrl = window.location.href;
-    if(newUrl.charAt(newUrl.length) != "/") {
-      newUrl = newUrl + "/";
-    }
-    newUrl = newUrl + $(this).data("contact-guid")
-    window.location.href = newUrl;
-  });
-};
+
+		function makeContactRowClickable() {
+		  $(".table > tbody > tr").click(function() {
+		    var newUrl = window.location.href;
+		    if(newUrl.charAt(newUrl.length) != "/") {
+		      newUrl = newUrl + "/";
+		    }
+		    newUrl = newUrl + $(this).data("contact-guid")
+		    window.location.href = newUrl;
+		  });
+		};
+
